e7e09be2fbfce217b4d3622c60989cb621117acc
mago <markus.schneider84@gmail.com>
2020-01-12 11:45:53 +0100

    Initial Project Setup

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# GodotWildJam17
+# NbKickstarter
+Kickstart Project
+
+
+## About
+This is a simple kickstarter project for NimbleBeasts projects. It comes with some standard features and little helpers as well as a some stuff, no one has time for in a game jam.
+
+## Structure
+
+- assets -> All the resources needed
+  - art -> All the art stuff
+  - fonts -> Fonts
+  - sounds -> Obviously sounds :D
+
+- src -> Scenes and Sources
+  - autoloads -> Singletons: types -> global -> debug
+  - Levels -> (Can be removed if static level)
+  - Menu -> Menu Scene and Code
+
+## Game Manager
+The idea is to get rid of most get_parent() cascade and introduce a game manager which holds most of the stuff. The game manager is accessible by the global singleton, e.g:
+Global.gm.loadLevel(0)
+or
+Global.getGameManager().loadLevel(0)
+
+## Starting Point
+By default src/Levels/Level0.tscn is added to the gameViewport and serves as a first starting point.
+Tipp: Hide the BootSplash node in the GameManager scene for faster loading :)
+
+
---
+++ src/autoloads/Global.gd
@@ -0,0 +1,146 @@
+###############################################################################
+# (c) NimbleBeasts 2019 and counting
+# This is template file
+###############################################################################
+
+# ToDos:
+# TODO goes here
+
+extends Node
+
+
+###############################################################################
+# Data
+###############################################################################
+
+# Version
+const version = {
+	"major": 0,
+	"minor": 1
+}
+
+# Nb Plugin Config
+const NbConfig = {
+	"gameId": "gameId",
+	"filePassword": "password",
+	"magic": "magiccode"
+}
+
+# User Config
+var userConfig = {
+	"highscore": 0
+}
+
+# GameMasterNode
+var gm = null
+var cam = null
+var hud = null
+var menu = null
+
+# Debug Label
+var debugLabel = null
+var debug = true
+
+# RNG base
+var rng = RandomNumberGenerator.new()
+var stateSeed = int(3458764513820540928)
+
+###############################################################################
+# Functions
+###############################################################################
+
+func _ready():
+	print("Starting: " + str(ProjectSettings.get_setting("application/config/name")) + " v" + getVersionString())
+	rng.randomize()
+	loadConfig()
+	videoSetup(2)
+
+# Config Save
+func saveConfig():
+	var cfgFile = File.new()
+	cfgFile.open("user://config.cfg", File.WRITE)
+	cfgFile.store_line(to_json(userConfig))
+	cfgFile.close()
+
+# Config Load
+func loadConfig():
+	var cfgFile = File.new()
+	if not cfgFile.file_exists("user://config.cfg"):
+		saveConfig()
+		return
+	
+	cfgFile.open("user://config.cfg", File.READ)
+	var data = parse_json(cfgFile.get_line())
+	userConfig.highscore = data.highscore
+
+# Window Scaler
+func videoSetup(scale = 2):
+	var initSize = Vector2(ProjectSettings.get_setting("display/window/size/width"), ProjectSettings.get_setting("display/window/size/height"))
+	var screen_size = OS.get_screen_size(OS.get_current_screen())
+	var window_size = initSize * scale
+	var centered_pos = (screen_size - window_size) / 2
+	OS.set_window_position(centered_pos)
+	OS.set_window_size(window_size)
+
+# Fullscreen Toggle
+func fullscreen():
+	if OS.window_fullscreen:
+		OS.window_fullscreen = false
+		videoSetup(2)
+	else:
+		videoSetup(3)
+		OS.window_fullscreen = true
+
+# PRNG
+func prng():
+	#TODO monte carlo simulation over rng
+	stateSeed = int((rng.randi() + 1) * stateSeed) + 1
+	return abs(stateSeed)
+
+# PRNG by Chance in Percentage
+func prngByChance(chanceInPercent):
+	var value = prng() % 100
+	if value <= chanceInPercent:
+		return true
+	return false
+
+
+###############################################################################
+# Getter/Setter
+###############################################################################
+
+# Get Version
+func getVersion():
+	return version
+	
+# Get Version String
+func getVersionString():
+	return "" + str(version.major) + "." + str(version.minor)
+
+# GameManager Set
+func setGameManager(node):
+	gm = node
+
+# GameManager Get
+func getGameManager():
+	return gm
+
+func setMenu(node):
+	menu = node
+
+func getMenu():
+	return menu
+
+func setHUD(node):
+	hud = node
+
+func getHUD():
+	return hud
+
+func getCam():
+	return cam
+
+func getDebug():
+	if debug:
+		return debugLabel
+	return null
