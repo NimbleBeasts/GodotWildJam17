644b2dd9f9a29bcbfd2b70c1a776e772a91c7078
mago <markus.schneider84@gmail.com>
2020-01-14 16:59:29 +0100

    Menu prototype

--- src/Menu/Menu.gd
+++ src/Menu/Menu.gd
@@ -1,34 +1,37 @@
 extends Control
 
-enum MenuState {Main, Settings}
+enum MenuState {Main, Settings, Credits, Multiplayer, Singleplayer}
 
 func _ready():
 	Global.setMenu(self)
 	$Version.bbcode_text = "[right]"+ Global.getVersionString() + "[/right]"
 	stateTransition(MenuState.Main)
 
-func _on_ButtonExit_button_up():
-	if Global.getGameManager().state == Types.GameStates.Menu:
-		print("Ok, Bye! Thanks for playing.")
-		get_tree().quit()
 
-
-func _on_ButtonSettings_button_up():
-	if Global.getGameManager().state == Types.GameStates.Menu:
-		stateTransition(MenuState.Settings)
-
-func _on_ButtonPlay_button_up():
-	if Global.getGameManager().state == Types.GameStates.Menu:
-		Global.getGameManager().newGame()
+func clearWindows():
+	$Main.hide()
+	$Settings.hide()
+	$Credits.hide()
+	$Multiplayer.hide()
+	$SinglePlayer.hide()
 
 func stateTransition(to):
-	if to == MenuState.Main:
-		$Settings.hide()
-		$Main.show()
-	elif to == MenuState.Settings:
-		updateSettings()
-		$Settings.show()
-		$Main.hide()
+	clearWindows()
+	match to:
+		MenuState.Main:
+			$Main.show()
+		MenuState.Settings:
+			updateSettings()
+			$Settings.show()
+		MenuState.Credits:
+			$Credits.show()
+		MenuState.Multiplayer:
+			$Multiplayer.show()
+		MenuState.Singleplayer:
+			$SinglePlayer.show()
+		_:
+			print("Unhandled Transition")
+
 
 func updateSettings():
 	var lights = Global.getGameManager().getLights()
@@ -39,6 +42,20 @@ func updateSettings():
 		$Settings/ButtonLights/Text.bbcode_text = "[center]Light: Off[/center]"
 
 
+func _on_ButtonExit_button_up():
+	if Global.getGameManager().state == Types.GameStates.Menu:
+		print("Ok, Bye! Thanks for playing.")
+		get_tree().quit()
+
+func _on_ButtonSettings_button_up():
+	if Global.getGameManager().state == Types.GameStates.Menu:
+		stateTransition(MenuState.Settings)
+
+func _on_ButtonPlay_button_up():
+	if Global.getGameManager().state == Types.GameStates.Menu:
+		stateTransition(MenuState.Singleplayer)
+
+
 func _on_ButtonBack_button_up():
 	stateTransition(MenuState.Main)
 
@@ -46,3 +63,31 @@ func _on_ButtonLights_button_up():
 	var lights = Global.getGameManager().getLights()
 	Global.getGameManager().setLights(!lights)
 	updateSettings()
+
+
+func _on_BtnDbg_button_up():
+	if Global.getGameManager().state == Types.GameStates.Menu:
+		Global.getGameManager().newGame()
+
+
+func _on_ButtonCredits_button_up():
+	if Global.getGameManager().state == Types.GameStates.Menu:
+		stateTransition(MenuState.Credits)
+
+
+func _on_ButtonMp_button_up():
+	if Global.getGameManager().state == Types.GameStates.Menu:
+		stateTransition(MenuState.Multiplayer)
+
+
+# Multiplayer Related
+func _on_ButtonLocal_button_up():
+	pass # Replace with function body.
+
+
+func _on_ButtonHost_button_up():
+	pass # Replace with function body.
+
+
+func _on_ButtonJoin_button_up():
+	pass # Replace with function body.
